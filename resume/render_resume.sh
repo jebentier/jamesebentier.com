15bf9dbc5c8b8798f191e4dca591aa5c630ebf01
James Ebentier <jebentier@gmail.com>
2021-07-29 18:00:49 +0200

    Update resume image to latest version of resume-cli

--- resume/render_resume.sh
+++ resume/render_resume.sh
@@ -3,9 +3,9 @@
 mode=$1
 
 case $mode in
-    "json") yaml2json resume.yml --pretty;;
-    "serve") yaml2json resume.yml --pretty > resume.json && resume serve --theme elegant;;
-    "html") yaml2json resume.yml --pretty > resume.json && resume export --format html --theme stackoverflow dist/resume.html;;
+    "json") yaml2json resume.yml -p;;
+    "serve") yaml2json resume.yml -p > resume.json && resume serve --theme /usr/local/lib/node_modules/jsonresume-theme-elegant;;
+    "html") yaml2json resume.yml -p > resume.json && resume export --format html --theme /usr/local/lib/node_modules/jsonresume-theme-stackoverflow dist/resume.html;;
     "yml") cat resume.yml;;
     *) echo "Please utilize a valid command (json|serve|yml|html)";;
 esac
