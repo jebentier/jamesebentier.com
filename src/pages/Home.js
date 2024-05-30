5c97f35abf24d24310eb16734ff95a9b3ac789cb
James Ebentier <jebentier@gmail.com>
2024-05-30 12:01:27 +0200

    Update landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,11 @@ export default function Home() {
       <div className='bio'>
         <p>
           Hello, my name is <strong>James Ebentier.</strong><br />
-          I am a <strong>Principle Software Engineer</strong> for <a href='https://invoca.com' target='_blank'>Invoca Inc.</a><br/>
+          I am a <strong>Software Architect</strong> for <a href='https://invoca.com' target='_blank'>Invoca Inc.</a><br/>
           based out of <strong>Berlin, Germany.</strong><br/>
-          Outside of work I am a <strong>mentor</strong> to any and all who seek it.
+          Outside of work I am a <strong>hacker</strong>, community <strong>advocate</strong>, and <strong>mentor</strong> to any and all who seek it.
         </p>
-        <img src='https://res.cloudinary.com/practicaldev/image/fetch/s--P-zvMTgt--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/qyix6eyhrnc8x9c44yp2.jpg' alt='James Ebentier Banner' />
+        <img src='https://images.pexels.com/photos/358312/pexels-photo-358312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='James Ebentier Banner' />
       </div>
     </div>
   );
